1823efcf600802fc175f9527ef02a316a9511eed
vivexatech-code <vivexatech.com>
2026-09-06 13:39:36 +0530

    v1

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -1,25 +1,26 @@
 import { ImageResponse } from "next/og";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
 
 export const size = { width: 180, height: 180 };
 export const contentType = "image/png";
 
-export default function AppleIcon() {
+export default async function AppleIcon() {
+  const svg = await readFile(join(process.cwd(), "public/logo.svg"));
+  const src = `data:image/svg+xml;base64,${svg.toString("base64")}`;
+
   return new ImageResponse(
     (
       <div
         style={{
-          width: "100%",
-          height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#07101c",
-          color: "#00c2e0",
-          fontSize: 96,
-          fontWeight: 700,
+          width: "100%",
+          height: "100%",
         }}
       >
-        V
+        <img src={src} alt="Vivexa Tech" width={180} height={180} />
       </div>
     ),
     size,
